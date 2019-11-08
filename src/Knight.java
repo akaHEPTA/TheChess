ac2b3de206ac202feb78d7bd153556a2d444ebfd
Anzu <anzunakayama@gmail.com>
2019-11-08 13:34:07 -0800

    Knight finished - Anzu

--- src/Knight.java
+++ src/Knight.java
@@ -8,17 +8,87 @@ public class Knight extends Piece {
 
     @Override
     protected boolean move(Position newPosition, Piece[][] board) {
-        return false;
+        boolean result = false;
+        // It should check super's validity (grid range) && Knight's validity
+        if (super.isValidMove(newPosition, board) && isValidMove(newPosition, board)) {
+            this.position = newPosition;
+            result = true;
+        }
+        return result;
     }
 
     @Override
     protected boolean isValidMove(Position newPosition, Piece[][] board) {
-        return false;
+        boolean result = false;
+        ArrayList<Position> validMoveList = getValidMoveList(board);
+        for (Position position : validMoveList)
+            if (position.equals(newPosition)) result = true;
+        return result;
     }
 
     @Override
-    public ArrayList<Position> getValidMoveList(Piece[][] Board) {
-        return null;
+    public ArrayList<Position> getValidMoveList(Piece[][] board) {
+        ArrayList<Position> validPositions = new ArrayList<>();
+        int rowPosition = this.position.getRow();
+        int colPosition = this.position.getCol();
+
+        // 1 o'clock position
+        if (rowPosition + 2 >= 0 && rowPosition + 2 < 8 && colPosition + 1 >= 0 && colPosition + 1 < 8) {
+            if (board[rowPosition + 2][colPosition + 1] == null
+                    || board[rowPosition + 2][colPosition + 1].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition + 2, colPosition + 1));
+            }
+        }
+        // 2 o'clock position
+        if (rowPosition + 1 >= 0 && rowPosition + 1 < 8 && colPosition + 2 >= 0 && colPosition + 2 < 8) {
+            if (board[rowPosition + 1][colPosition + 2] == null
+                    || board[rowPosition + 1][colPosition + 2].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition + 1, colPosition + 2));
+            }
+        }
+        // 4 o'clock position
+        if (rowPosition - 1 >= 0 && rowPosition - 1 < 8 && colPosition + 2 >= 0 && colPosition + 2 < 8) {
+            if (board[rowPosition - 1][colPosition + 2] == null
+                    || board[rowPosition - 1][colPosition + 2].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition - 1, colPosition + 2));
+            }
+        }
+        // 5 o'clock position
+        if (rowPosition - 2 >= 0 && rowPosition - 2 < 8 && colPosition + 1 >= 0 && colPosition + 1 < 8) {
+            if (board[rowPosition - 2][colPosition + 1] == null
+                    || board[rowPosition - 2][colPosition + 1].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition - 2, colPosition + 1));
+            }
+        }
+        // 7 o'clock position
+        if (rowPosition - 2 >= 0 && rowPosition - 2 < 8 && colPosition - 1 >= 0 && colPosition - 1 < 8) {
+            if (board[rowPosition - 2][colPosition - 1] == null
+                    || board[rowPosition - 2][colPosition - 1].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition - 2, colPosition - 1));
+            }
+        }
+        // 8 o'clock position
+        if (rowPosition - 1 >= 0 && rowPosition - 1 < 8 && colPosition - 2 >= 0 && colPosition - 2 < 8) {
+            if (board[rowPosition - 1][colPosition - 2] == null
+                    || board[rowPosition - 1][colPosition - 2].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition - 1, colPosition - 2));
+            }
+        }
+        // 10 o'clock position
+        if (rowPosition + 1 >= 0 && rowPosition + 1 < 8 && colPosition - 2 >= 0 && colPosition - 2 < 8) {
+            if (board[rowPosition + 1][colPosition - 2] == null
+                    || board[rowPosition + 1][colPosition - 2].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition + 1, colPosition - 2));
+            }
+        }
+        // 11 o'clock position
+        if (rowPosition - 1 >= 0 && rowPosition - 1 < 8 && colPosition + 2 >= 0 && colPosition + 2 < 8) {
+            if (board[rowPosition - 1][colPosition + 2] == null
+                    || board[rowPosition - 1][colPosition + 2].isWhite != this.isWhite) {
+                validPositions.add(new Position(rowPosition - 1, colPosition + 2));
+            }
+        }
+        return validPositions;
     }
 
     @Override
--- src/Queen.java
+++ src/Queen.java
@@ -20,7 +20,6 @@ public class Queen extends Piece {
             this.position = newPosition;
             result = true;
         }
-
         return result;
     }
 
